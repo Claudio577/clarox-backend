07b0d24251fdf904cb5c45d2df3d30f814c34ec1
Claudio577 <chideki.yoshida@gmail.com>
2025-11-25 09:03:39 -0300

    Create internet_status.py

---
+++ services/internet_status.py
@@ -0,0 +1,6 @@
+def verificar_internet(user_id: str):
+    return {
+        "user_id": user_id,
+        "status": "instável",
+        "descricao": "Detectei instabilidade na sua região, mas já estamos resolvendo."
+    }
